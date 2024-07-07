52962aaf93afbf68e9b7d13813be9514c7fb3483
名字 <xyb12345678qwe@hotmail.com>
2024-07-07 16:57:09 +0800

    更新压缩功能

--- github更新.bat
+++ github更新.bat
@@ -3,5 +3,5 @@ git remote add github https://github.com/xyb12345678qwe/random-image-koa.git
 git init
 git add .
 git branch -M main
-git commit -m "更新"
+git commit -m "更新压缩功能"
 git push -u github main
